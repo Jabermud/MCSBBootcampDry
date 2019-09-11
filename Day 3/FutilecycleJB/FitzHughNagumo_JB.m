78bedbfb9afe4de87c21daa566271e8a3ce84e2f
jabermud <jabermud>
2019-09-11 16:26:47 -0700

    This commit updates FitzHughNagumo

--- Day 3/FutilecycleJB/FitzHughNagumo_JB.m
+++ Day 3/FutilecycleJB/FitzHughNagumo_JB.m
@@ -5,9 +5,10 @@ b=0.2;
 dVdt=@(v,w) v-(1/3)*v^3-w
 dWdt=@(v,w) eps(v+a-b*w)
 
-
+FHN=@(t,x)
 % solve system
-[T,X] = ode45(dVdt,[0,100],[a,b]);
+[T,X] = ode45(dVdt,[0,100],[a,b]...
+    );
 figure(1); clf; hold on; box on;
 plot(T,X,'LineWidth',2);
 plot(T,sum(X,2),'--k','LineWidth',2);
