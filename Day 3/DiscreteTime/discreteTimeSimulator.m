872bd1a01f95a88ee3c30628739f136457fb9b67
jabermud <jabermud>
2019-09-11 10:42:22 -0700

    'jabermud discrete time'

--- Day 3/DiscreteTime/discreteTimeSimulator.m
+++ Day 3/DiscreteTime/discreteTimeSimulator.m
@@ -3,15 +3,21 @@
 % x- population of RABBITS in thousands
 % n- time in WEEKS
 
-nMax = 20;% max number of days to simulate
-x = ones(1,nMax); % population
-x(1) = 1.0; % initial condition
-
-for n=2:nMax
+K=0.6
+nMax = 100;% max number of days to simulate
+x = ones(7,nMax); % population
+x(:,1) = 0.2; % initial condition
+r_all=0:.5:3;
+for row=1:7
+    r=r_all(row);
+    for n=2:nMax
     
-    x(n) = x(n-1) + 5;
+    x(row,n) = x(row,n-1)+r*(1- (x(row,n-1)/K))*x(row,n-1);
     
+    end
+   %plot
 end % finished loop through days
+plot(r_all,x(end-3,end))
 
 figure;
 plot(x,'-ok');
